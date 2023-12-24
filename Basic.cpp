0a0a8917520e70d50aaaf293cef94a4d4039a57f
Yash L <147707204+YashL3616@users.noreply.github.com>
2023-12-24 09:34:22 +0530

    Update Basic.cpp

--- Basic.cpp
+++ Basic.cpp
@@ -52,3 +52,46 @@ int main()
     call_friend();
     return 0;
 }
+
+
+//accessing 2 variables in 2 diff class
+//first store using public then use in calling func
+#include<iostream>
+using namespace std;
+class alpha
+{
+    private:
+    int frndVar;
+    
+    public:
+    alpha(int use=0)  //constructor
+    {
+        frndVar=use;
+    }
+    friend void call_friend();
+};
+class beta
+{
+    private:
+    int frndVar2;
+    
+    public:
+    beta(int use2=0)  //constructor
+    {
+        frndVar2=use2;
+    }
+    friend void call_friend();
+};
+
+void call_friend()
+{
+    alpha anyVar(10);
+    beta anyVar2(20);
+    int x=anyVar.frndVar+anyVar2.frndVar2;
+    cout<<x;
+}
+int main()
+{
+    call_friend();
+    return 0;
+}
