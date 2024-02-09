4f7c2f2824fa32d70806158285a4265de715550f
Yash L <147707204+YashL3616@users.noreply.github.com>
2024-02-09 13:35:26 +0530

    AddDigits.cpp

--- AddDigits.cpp
+++ AddDigits.cpp
@@ -2,7 +2,8 @@
 using namespace std;
 class Solution {
 public:
-    int addDigits(int num) {
+    int addDigits(int num) 
+  {
         while (num >= 10) 
         {   int sum = 0;
             while (num > 0)
